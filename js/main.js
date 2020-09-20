184ecbb673c0608b39cee07fcf5d7dabca6ba7e7
Vokhidov <vokhidov610@gmail.com>
2020-09-20 21:37:50 +0500

    'started'

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    navText : ["<button class='carousel-icon'><i class='icon-left carousel-icon carousel-icon'></i></button>","<button class='carousel-icon'><i class='icon-right'></i></button>"],
+    items:1,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true,
+        }
+    }
+})
+
+$('.med-carousel').owlCarousel({
+    margin:10,
+    nav:true,
+    dots:true,
+    items:3,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true,
+        }
+    }
+})
